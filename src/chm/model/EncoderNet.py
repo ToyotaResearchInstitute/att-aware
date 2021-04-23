5258fbd5658414ba74c33caa313589db5699a72c
Deepak Gopinath <musicmagic@gmail.com>
2021-04-23 09:13:23 -0500

    Added s3d module. create encoder net WIP

--- src/chm/model/EncoderNet.py
+++ src/chm/model/EncoderNet.py
@@ -1,7 +1,66 @@
 import torch
+import torchvision.models
+
+from chm.model.S3D import STConv3d
 
 
 def create_encoder(reduced_middle_layer_size=512, use_s3d_encoder=False):
+    """
+    Creates the encoder structure. Two possible encoders.
+    1. With only ResNet18 encoder layers. 4 layers
+    2. When use_s3d_encoder is True, then 3 layers of S3D will be stacked on top of two layers of ResNet18. Total of 5
+
+    Parameters:
+    ----------
+    reduced_middle_layer_size: int
+        Number of feature layers for the Conv3D (1 x 1 x 1) after the encoder. Used for reduced the number of parameters
+    use_s3d_encoder: bool
+        Flag which decides the encoder structure
+    """
+
+    orig_net = torchvision.models.resnet18(pretrained=True)  # use resnet
+    # initial layers of resnet for preprocessing
+    preproc_net = torch.nn.Sequential()
+    preproc_net.add_module("conv1", orig_net.conv1)
+    preproc_net.add_module("bn1", orig_net.bn1)
+    preproc_net.add_module("relu1", orig_net.relu)
+    preproc_net.add_module("maxpool", orig_net.maxpool)
+
+    # encoder layers from resnet for 2d convs. Present for both nonS3D and S3D architectures
+    layered_net = torch.nn.ModuleDict()
+    layered_net["layer1"] = orig_net.layer1
+    layered_net["layer2"] = orig_net.layer2
+    if not use_s3d_encoder:
+        layered_net["layer3"] = orig_net.layer3
+        layered_net["layer4"] = orig_net.layer4
+        s3d_net = None
+        # encoder layers will be [layer1, layer2, layer3, layer4], None
+    else:
+        s3d_net = torch.nn.ModuleDict()
+        # the input dim for s3d net should match the output of layer2 of resnet.
+        s3d_net["s3d_net_1"] = STConv3d(
+            in_channels=list(orig_net.layer2.children())[-1].bn2.num_features,
+            out_channels=256,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        s3d_net["s3d_net_2"] = STConv3d(
+            in_channels=256,
+            out_channels=512,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        s3d_net["s3d_net_3"] = STConv3d(
+            in_channels=512,
+            out_channels=512,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        # encoder layers will be [layer1, layer2], [s3d_net_1, s3d_net_2, s3d_net_3]
+
     import IPython
 
-    IPython.embed(banner1="check in create encoder")
+    IPython.embed(banner1="check encoder")
---
+++ src/chm/model/S3D.py
@@ -0,0 +1,58 @@
+import torch
+
+
+class STConv3d(torch.nn.Module):
+    """
+    Adapted from https://github.com/qijiezhao/s3d.pytorch/blob/master/S3DG_Pytorch.py
+
+    S3D structure used in encoder and decoder. Consists of separate spatial and temporal Conv3D with (1, k, k) and (k, 1,1) kernels.
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding=0):
+        """
+        Parameters
+        ---------
+        in_channels: int
+            number of input features for the spatial conv3d
+        out_channels: int
+            number of output features for the spatial conv3d. This is also used as the number of
+            input and output features for the temporal conv3d
+        kernel_size: int
+            Convolution kernel size.
+        stride: int
+            Convolution stride length
+        padding: int
+            Convolution padding size
+        """
+
+        super(STConv3d, self).__init__()
+        # for spatial conv only pad spatial dim
+        self.spatial_replication_pad = torch.nn.ReplicationPad3d((0, 0, padding, padding, padding, padding))
+        # spatial conv (1, k, k)
+        self.spatial_conv3d = torch.nn.Conv3d(
+            in_channels,
+            out_channels,
+            kernel_size=(1, kernel_size, kernel_size),
+            stride=(1, stride, stride),
+            padding=(0, 0, 0),
+        )
+        # for temp dim only pad temporal dim
+        self.temporal_replication_pad = torch.nn.ReplicationPad3d((padding, padding, 0, 0, 0, 0))
+        # temporal conv (k, 1, 1)
+        self.temporal_conv3d = torch.nn.Conv3d(
+            out_channels, out_channels, kernel_size=(kernel_size, 1, 1), stride=(stride, 1, 1), padding=(0, 0, 0)
+        )
+        self.spatial_instance_norm = torch.nn.InstanceNorm3d(out_channels)
+        self.temporal_instance_norm = torch.nn.InstanceNorm3d(out_channels)
+
+        self.spatial_nonlin = torch.nn.ReLU()
+        self.temporal_nonlin = torch.nn.ReLU()
+
+    def forward(self, x):
+        """
+        assumes x is (B, C, T, H, W)
+        """
+
+        x = self.spatial_nonlin(self.spatial_instance_norm(self.spatial_conv3d(self.spatial_replication_pad(x))))
+        x = self.temporal_nonlin(self.temporal_instance_norm(self.temporal_conv3d(self.temporal_replication_pad(x))))
+        return x
