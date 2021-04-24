2cb57ff8add9c293877a3193e42a08fd6d219131
Deepak Gopinath <musicmagic@gmail.com>
2021-04-24 15:00:53 -0500

    convert labels to float

--- src/chm/dataset/chm_att_awareness_dataset.py
+++ src/chm/dataset/chm_att_awareness_dataset.py
@@ -119,6 +119,6 @@ class CHMAttAwarenessDataset(CHMBaseDataset):
         # append annotation info to the data_dict dictionary
         annotation_dict = att_label_item.to_dict()
         # convert awareness annotation into a float
-        annotation_dict["is_aware"] = self.convert_awareness_annotation_to_float(annotation_dict["is_aware"])
+        annotation_dict["anno_is_aware"] = self.convert_awareness_annotation_to_float(annotation_dict["anno_is_aware"])
         data_dict["att_annotation"] = annotation_dict
         return data_dict, auxiliary_info_dict
