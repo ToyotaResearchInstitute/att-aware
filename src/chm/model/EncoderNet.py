ab8d1d5b3fecced12ffa2c44b5bcf32b8bbe75fe
Deepak Gopinath <musicmagic@gmail.com>
2021-05-07 09:22:46 -0500

    refactor model classes

---
+++ src/chm/model/EncoderNet.py
@@ -0,0 +1,185 @@
+# Copyright 2020 Toyota Research Institute.  All rights reserved.
+import torch
+import torchvision.models
+import collections
+
+from chm.model.S3D import STConv3d
+
+
+def run_over_images(input, net, axis):
+    """
+    Parameters:
+    ----------
+    input: torch.Tensor
+        (B, T, C, H, W). Image sequence
+    net: torch.nn.Module
+        network to be used for processing input
+    axis: int
+        axis along which the input needs to be extract for individual application of network
+
+    Returns:
+    --------
+    output: torch.Tensor
+        Result of applying net on input
+    """
+    output = []
+    for i in range(input.shape[axis]):
+        # for the entire batch grab the ith image along axis by using index_select,
+        # remove the time dimension using squeeze, pass it through a network
+        # that expects a batch of images, and then finally add back the removed dimension
+        tmp = net(torch.index_select(input, dim=axis, index=input.new_tensor([i]).long()).squeeze(axis)).unsqueeze(axis)
+        output.append(tmp)
+    # concatenate the results along the time dimension.
+    # so its back to B * T * C * H * W
+    output = torch.cat(output, axis)
+    return output
+
+
+def create_encoder(reduced_middle_layer_size=512, use_s3d=False):
+    """
+    Creates the encoder structure. Two possible options for encoders.
+    1. With only ResNet18 encoder layers. 4 layers
+    2. When use_s3d is True, then 3 layers of S3D will be stacked on top of two layers of ResNet18. Total of 5.
+
+    Parameters:
+    ----------
+    reduced_middle_layer_size: int
+        Number of feature layers for the Conv3D (1 x 1 x 1) after the encoder. Used for reduced the number of parameters
+    use_s3d: bool
+        Flag which decides the encoder structure
+
+    Returns:
+    -------
+    EncoderNet: torch.nn.Module
+        Encoder network for CHM
+    """
+
+    orig_net = torchvision.models.resnet18(pretrained=True)  # use resnet
+    # initial layers of resnet for preprocessing
+    preproc_net = torch.nn.Sequential()
+    preproc_net.add_module("conv1", orig_net.conv1)
+    preproc_net.add_module("bn1", orig_net.bn1)
+    preproc_net.add_module("relu1", orig_net.relu)
+    preproc_net.add_module("maxpool", orig_net.maxpool)
+
+    # encoder layers from resnet for 2d convs. Present for both nonS3D and S3D architectures
+    layered_net = torch.nn.ModuleDict()
+    layered_net["layer1"] = orig_net.layer1
+    layered_net["layer2"] = orig_net.layer2
+    if not use_s3d:
+        layered_net["layer3"] = orig_net.layer3
+        layered_net["layer4"] = orig_net.layer4
+        s3d_net = None
+        # encoder layers will be [layer1, layer2, layer3, layer4], None
+    else:
+        s3d_net = torch.nn.ModuleDict()
+        s3d_layer_3_out_channels = 512
+        # the input dim for s3d net should match the output of layer2 of resnet.
+        s3d_net["s3d_net_1"] = STConv3d(
+            in_channels=list(orig_net.layer2.children())[-1].bn2.num_features,
+            out_channels=256,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        s3d_net["s3d_net_2"] = STConv3d(
+            in_channels=256,
+            out_channels=512,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        s3d_net["s3d_net_3"] = STConv3d(
+            in_channels=512,
+            out_channels=s3d_layer_3_out_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        # encoder layers will be [layer1, layer2], [s3d_net_1, s3d_net_2, s3d_net_3]
+
+    post_processing = None
+    post_processing_bn = None
+    post_processing_nonlin = None
+    if reduced_middle_layer_size is not None:
+        if not use_s3d:
+            orig_latent_dim = list(orig_net.layer4.children())[-1].conv2.out_channels
+        else:
+            orig_latent_dim = s3d_layer_3_out_channels  # out_channels in s3d_net['s3d_net_3']
+
+        post_processing = torch.nn.Conv3d(orig_latent_dim, reduced_middle_layer_size, 1)
+        post_processing_in = torch.nn.InstanceNorm3d(reduced_middle_layer_size)
+        post_processing_nonlin = torch.nn.ReLU()
+
+    post_layers = {}
+    post_layers["conv3d"] = post_processing
+    post_layers["in"] = post_processing_in
+    post_layers["relu"] = post_processing_nonlin
+
+    return EncoderNet(preproc_net=preproc_net, layered_outputs=layered_net, s3d_net=s3d_net, post_layers=post_layers)
+
+
+class EncoderNet(torch.nn.Module):
+    """
+    Class for the Encoder Net in CHMNet
+    """
+
+    def __init__(self, preproc_net, layered_outputs, s3d_net=None, post_layers=None):
+        """
+        Parameters:
+        -----------
+        preproc_net: torch.nn.Sequential
+            Initialize preprocessing modules for encoder
+        layered_output: torch.nn.ModuleDict
+            Initial layers of the encoder. Either [layer1, layer2] or [layer1, layer2...layer4]
+        s3d_net: torch.nn.ModuleDict or None
+            If use_s3d flag was true the s3d_net consists of [s3d_net_1, s3d_net_2, s3d_net_3]. else None
+        post_layers: dict or None
+            dict containing the various layers for post processing
+        """
+        super().__init__()
+        self.preproc_net = preproc_net
+        self.layered_outputs = layered_outputs
+        self.s3d_net = s3d_net
+        self.post_layers = post_layers
+        if self.post_layers is not None:
+            self.post_layer_conv3d = self.post_layers["conv3d"]
+            self.post_layer_in = self.post_layers["in"]
+            self.post_layer_relu = self.post_layers["relu"]
+
+    def forward(self, input):
+        """
+        Encode an input (or a sequence of input) image(s) via a set of convolutions and subsamples, save the intermediate outputs for shortcut
+        connections.
+
+        Parameters:
+        ----------
+        input: torch.Tensor
+            input (or sequence of) images
+
+        Returns:
+        -------
+        encoder_output: OrderedDict()
+            a dictionary containing all the intermediate results of applying fwd() on the image or batch of images
+        """
+        intermediate_output = run_over_images(input, self.preproc_net, axis=1)
+        encoder_output = collections.OrderedDict()
+        for key in self.layered_outputs:  # keys are [layer1, layer2, [layer3, layer4]]
+            encoder_output[key] = run_over_images(intermediate_output, self.layered_outputs[key], axis=1)
+            intermediate_output = encoder_output[key]
+
+        if self.s3d_net is not None:
+            for key in self.s3d_net:
+                intermediate_output = intermediate_output.permute(0, 2, 1, 3, 4)  # B, T, C, H, W --> B, C, T, H, W
+                out = self.s3d_net[key](intermediate_output)  # (B, C, T, H, W)
+                encoder_output[key] = out.permute(0, 2, 1, 3, 4)  # (B, T, C, H, W)
+                intermediate_output = encoder_output[key]
+
+        if self.post_layers is not None:
+            # from [B,T,C,H,W] to [B,C,T,H,W] for 3D conv, and back.
+            intermediate_output = self.post_layer_relu(
+                self.post_layer_in(self.post_layer_conv3d(encoder_output[key].transpose(1, 2)))
+            ).transpose(1, 2)
+            encoder_output[key] = intermediate_output
+
+        return encoder_output
---
+++ src/chm/model/gaze_transform.py
@@ -0,0 +1,134 @@
+# Copyright 2020 Toyota Research Institute.  All rights reserved.
+import torch
+import numpy as np
+
+
+def compute_inverted_affine_transform(linear_mtx, trans_vec):
+    """
+    Parameters:
+    -----------
+    linear_mtx: np.matrix
+        The A matrix of an affine transform of the form y = Ax + b
+    trans_vec: np.array
+        The b vector of an affine transform of the form y = Ax + b
+
+    Returns:
+    --------
+    inverted_affine_transform: tuple
+        tuple containing A matrix and b vector of the inverse transform. This is computed as
+        for  y = Ax + b ---> x = A^-1( y - b) --> A' = A^-1 and b' = -(A^-1)*b
+    """
+    result_mtx = np.linalg.pinv(linear_mtx)
+    result_vec = -np.dot(result_mtx, trans_vec)
+    inverted_affine_transform = (result_mtx, result_vec)
+    return inverted_affine_transform
+
+
+class GazeTransform(torch.nn.Module):
+    def __init__(self, scale_factor=1.0, linear_transform=None, translation=None, pad_gaze_vector=True):
+        """
+        A module to capture the affine transformation of the gaze modeled as y=Ax + b
+        This should be used either to correct the transformation,
+        or to corrupt it.
+
+        Parameters:
+        ----------
+        scale_factor: float
+            Factor by which the A matrix is stretched/compressed along the main diagonal
+        linear_transform: np.matrix
+            2 by 2 matrix containing the linear part of transform
+        translation: np.array
+            2 by 1 array containing the bias part of the transform
+        pad_gaze_vector: bool
+            Bool indicating whether the should_train_bit and the dropout bit should be added to the gaze tensor
+
+        """
+        super().__init__()
+        # the weight matrix associated with network is a 2 by 2 matrix since gaze points are
+        # always in 2D.
+        self.lin_trans = torch.nn.Linear(2, 2)
+        self.lin_trans.weight.data.normal_(mean=0, std=0.00001)
+        self.lin_trans.bias.data.normal_(mean=0, std=0.00001)
+        self.scale_factor = scale_factor
+        self.linear_transform = linear_transform
+        self.translation = translation
+        assert not (
+            (linear_transform is None) ^ (translation is None)
+        ), "linear_transform and the translation vector should be either both None or both instantiated"
+
+        if not (linear_transform is None):
+            assert tuple(self.lin_trans.weight.shape) == linear_transform.shape
+            assert tuple(self.lin_trans.bias.shape) == translation.shape
+            self.lin_trans.weight = torch.nn.Parameter(self.lin_trans.weight.new_tensor(linear_transform))
+            self.lin_trans.bias = torch.nn.Parameter(self.lin_trans.bias.new_tensor(translation))
+        else:
+            for i in range(2):
+                self.lin_trans.weight.data[i][i] += scale_factor
+
+        self.pad_gaze_vector = pad_gaze_vector
+
+    def forward(self, input, should_train_input_gaze=False):
+        """
+        Parameters:
+        -----------
+        input: torch.Tensor
+            Tensor of shape (B, T, L, 2) containing the gaze points
+        should_train_input_gaze: bool
+            Flag indicating whether the should train input gaze bit be set after transformation
+
+        Returns:
+        --------
+        transformed_output: torch.Tensor
+            Transform gaze tensor. (B, T, L, 2) without should train and dropout bits, (B, T, L, 4) otherwise
+        """
+
+        # Change from (B, T, L, 2) ---> (B, TL, 2)
+        input_reshaped = input.reshape(input.shape[0], input.shape[1] * input.shape[2], -1)
+
+        transformed_output_reshaped = self.lin_trans(input_reshaped)  # (B, TL, 2)
+        # (B, T, L, 2)
+        transformed_output = transformed_output_reshaped.reshape(input.shape[0], input.shape[1], input.shape[2], -1)
+        # adds a ones-all element to the last dim, to state the gaze input is there
+        if self.pad_gaze_vector:
+            # return (B, T, L, 4)
+            return torch.cat(
+                [
+                    transformed_output,
+                    should_train_input_gaze,
+                    transformed_output.new_ones(transformed_output[:, :, :, 0:1].shape),
+                ],
+                dim=3,
+            )
+        else:
+            # return (B, T, L, 2)
+            return transformed_output
+
+    def prior_loss(self):
+        """
+        Function that computes the regularization term for the gaze transform.
+
+        Parameters:
+        ----------
+        None
+
+        Returns:
+        -------
+        gaze_transform_loss: float
+            Computed element-wise squared error on the weights and biases of the gaze transform module
+        """
+        gaze_transform_loss = 0.0
+        if not (self.linear_transform is None):
+            for i in range(2):
+                gaze_transform_loss += (self.lin_trans.bias.data[i] - self.translation[i]) ** 2
+                for j in range(2):
+                    mean = self.linear_transform[i][j]
+                    gaze_transform_loss += (self.lin_trans.weight.data[i][j] - mean) ** 2
+
+        else:
+            for i in range(2):
+                gaze_transform_loss += (self.lin_trans.bias.data[i]) ** 2
+                for j in range(2):
+                    mean = self.scale_factor if i == j else 0
+                    gaze_transform_loss += (self.lin_trans.weight.data[i][j] - mean) ** 2
+
+        return gaze_transform_loss
