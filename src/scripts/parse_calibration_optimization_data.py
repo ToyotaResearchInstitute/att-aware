195537f4ec438beccee61e14781aab4ece67fd70
Deepak Gopinath <musicmagic@gmail.com>
2021-05-10 08:52:35 -0500

    data parsing scripts

---
+++ src/scripts/parse_calibration_optimization_data.py
@@ -0,0 +1,87 @@
+# Copyright 2020 Toyota Research Institute.  All rights reserved.
+import uuid
+import json
+import os
+import numpy as np
+
+from chm.configs.args_file import parse_arguments
+from chm.utils.experiment_result_keys import *
+
+"""
+Script to extract error stats from calibration optimization experiments. 
+The results of the experiments are expected to stored in files with the following filename convention
+
+experiment_type_gaze_calibration_miscalibration_noise_level_NOISELEVEL_optimization_run_num_OPTIMIZATIONNUM_FILENAMEAPPEND.json,
+where NOISELEVEL is in the miscalibration_noise_levels arg in experiment_chm_calibration_optimization
+OPTIMIZATIONNUM goes from 0 to num_optimization_runs-1, 
+and FILENAMEAPPEND is the 'filename_append' arg in the experiment. 
+
+Usage:
+python parse_denoising_data.py --folder_containing_results FOLDER_CONTAINING_JSONS --num_optimization_runs (same val as used in the experiment)
+--miscalibration_noise_levels (same val as used in the experiment) --filename_append (same val as used in the experiment)
+"""
+
+
+def arg_setter(parser):
+    parser.add_argument(
+        "--folder_containing_results",
+        type=str,
+        default="",
+        help="Path to folder containing calibration optimization results",
+    )
+
+    parser.add_argument(
+        "--num_optimization_runs",
+        type=int,
+        default=15,
+        help="Number of optimizations runs performed for a given noise level for the calibration experiment",
+    )
+
+    parser.add_argument(
+        "--miscalibration_noise_levels",
+        action="store",
+        nargs="*",
+        type=float,
+        default=[0.1, 0.2, 0.3, 0.5],
+        help="Noise levels used in the calibration experiments",
+    )
+
+    parser.add_argument(
+        "--filename_append", type=str, default="", help="Additional descriptive string for filename string components"
+    )
+
+
+if __name__ == "__main__":
+    session_hash = uuid.uuid4().hex
+    args = parse_arguments(session_hash, [arg_setter])
+    miscalibration_noise_levels = args.miscalibration_noise_levels
+    num_optimization_runs = args.num_optimization_runs
+    filename_append = args.filename_append
+    file_prefix = "experiment_type_gaze_calibration_miscalibration_noise_level_"
+    for noise_level in miscalibration_noise_levels:
+        starting_mse_error_list = []
+        end_mse_error_list = []
+        for i in range(num_optimization_runs):
+            jsnfile_name = (
+                file_prefix + str(noise_level) + "_optimization_run_num_" + str(i) + filename_append + ".json"
+            )
+            print("Loading : ", jsnfile_name)
+            jsn_full_path = os.path.join(args.folder_containing_results, jsnfile_name)
+            with open(jsn_full_path, "r") as fp:
+                res = json.load(fp)
+
+            starting_mse_error_list.append(res["errors"][0]["error"])
+            # assumes that the calibration optimization successfully converged
+            end_mse_error_list.append(res["errors"][-1]["error"])
+
+        print(
+            "Mean and std for starting error for noise level {} is {} and {}".format(
+                noise_level, np.mean(starting_mse_error_list), np.std(starting_mse_error_list)
+            )
+        )
+        print(
+            "Mean and std for ending error for noise level {} is {} and {}".format(
+                noise_level, np.mean(end_mse_error_list), np.std(end_mse_error_list)
+            )
+        )
+        print("              ")
---
+++ src/scripts/parse_denoising_data.py
@@ -0,0 +1,72 @@
+# Copyright 2020 Toyota Research Institute.  All rights reserved.
+import uuid
+import os
+import glob
+import json
+import numpy as np
+
+from chm.configs.args_file import parse_arguments
+from chm.utils.experiment_result_keys import *
+
+"""
+Script to extract error stats from gaze denoising experiments. If the file to be parsed is named
+experiment_type_gaze_denoising_EXTRA_INFO.json, the script is to be used as follows
+
+Usage:
+python parse_denoising_data.py --results_json_prefix experiment_type_gaze_denoising_
+"""
+
+
+def arg_setter(parser):
+    parser.add_argument(
+        "--results_json_prefix",
+        type=str,
+        default="",
+        help="Prefix for data json files from awareness estimation experiments",
+    )
+
+
+if __name__ == "__main__":
+    session_hash = uuid.uuid4().hex
+    args = parse_arguments(session_hash, [arg_setter])
+    # extract all the data files with the specified prefix and sort them
+    filenames = glob.glob(os.path.expanduser(args.results_json_prefix) + "*.json")
+    filenames = sorted(filenames)
+    for filename in filenames:
+        print("     ")
+        print(filename)
+        with open(filename, "r") as fp:
+            jsn = json.load(fp)
+
+        # grab errors computed during experiment.
+        error_noisy = jsn[GAZE_ERROR_NOISY_KEY]
+        error_objectbased = jsn[GAZE_ERROR_OBJ_KEY]
+        error_chm = jsn[GAZE_ERROR_CHM_KEY_WITH_GAZE]
+        error_saliency = jsn[GAZE_ERROR_CHM_KEY_WITHOUT_GAZE]
+
+        # grab the mean sqrt errors.
+        sqrt_error_noisy = jsn["overall_" + GAZE_ERROR_NOISY_KEY + "_sqrt_mean"]
+        sqrt_error_objectbased = jsn["overall_" + GAZE_ERROR_OBJ_KEY + "_sqrt_mean"]
+        sqrt_error_chm = jsn["overall_" + GAZE_ERROR_CHM_KEY_WITH_GAZE + "_sqrt_mean"]
+        sqrt_error_saliency = jsn["overall_" + GAZE_ERROR_CHM_KEY_WITHOUT_GAZE + "_sqrt_mean"]
+
+        # compute mean and std of errors
+        mean_noisy = np.average(error_noisy)
+        mean_object = np.average(error_objectbased)
+        mean_chm = np.average(error_chm)
+        mean_saliency = np.average(error_saliency)
+        std_noisy = np.std(error_noisy)
+        std_object = np.std(error_objectbased)
+        std_chm = np.std(error_chm)
+        std_saliency = np.std(error_saliency)
+        # print the mean and std errors for different denoising approaches. "Noisy" refers to without denoising
+        print(
+            "CHM: {} +/- {}. Saliency: {} +/- {}. Object-based: {} +/- {}. Noisy: {} +/- {}".format(
+                mean_chm, std_chm, mean_saliency, std_saliency, mean_object, std_object, mean_noisy, std_noisy
+            )
+        )
+        print(
+            "SQRT MEAN CHM: {}. Saliency: {}. Object-based: {}. Noisy: {}".format(
+                sqrt_error_chm, sqrt_error_saliency, sqrt_error_objectbased, sqrt_error_noisy
+            )
+        )
